7e8c966ebbbf376d1bbc88bccac176b749f6afcc
jbyannaccone2015 <justin@yannaccone.net>
2021-02-04 12:29:29 -0500

    Added image and meaning functionaltiy

--- src/Components/Deck/Deck-array.js
+++ src/Components/Deck/Deck-array.js
@@ -1,82 +1,476 @@
+import world from './tarot_cards/THE_WORLD.JPG'
+import judgement from './tarot_cards/JUDGEMENT.JPG'
+import sun from './tarot_cards/THE_SUN.JPG'
+import moon from './tarot_cards/THE_MOON.JPG'
+import star from './tarot_cards/THE_STAR.JPG'
+import tower from './tarot_cards/THE_TOWER.JPG'
+import devil from './tarot_cards/THE_DEVIL.JPG'
+import temperance from './tarot_cards/TEMPERANCE.jpg'
+import death from './tarot_cards/DEATH.JPG'
+import hanged from './tarot_cards/THE_HANGED_MAN.JPG'
+import justice from './tarot_cards/JUSTICE.JPG'
+import wheel from  './tarot_cards/WHEEL_OF_FORTUNE.JPG'
+import hermit from './tarot_cards/THE_HERMIT.JPG'
+import strength from './tarot_cards/STRENGTH.JPG'
+import chariot from './tarot_cards/THE_CHARIOT.JPG'
+import lovers from './tarot_cards/THE_LOVERS.JPG'
+import heirophant from './tarot_cards/THE_HEIROPHANT.JPG'
+import emperor from './tarot_cards/THE_EMPEROR.JPG'
+import empress from './tarot_cards/THE_EMPRESS.JPG'
+import priestess from './tarot_cards/THE_HIGH_PRIESTESS.JPG'
+import magician from './tarot_cards/THE_MAGICIAN.JPG'
+import fool from './tarot_cards/THE_FOOL.JPG'
+import sevenWand from './tarot_cards/SEVEN_OF_WANDS.JPG'
+import fourWand from './tarot_cards/FOUR_OF_WANDS.JPG'
+import aceWand from './tarot_cards/ACE_OF_WANDS.JPG'
+import tenWand from './tarot_cards/TEN_OF_WANDS.JPG'
+import nineWand from './tarot_cards/NINE_OF_WANDS.JPG'
+import eightWand from './tarot_cards/EIGHT_OF_WANDS.JPG'
+import sixWand from './tarot_cards/SIX_OF_WANDS.JPG'
+import fiveWand from './tarot_cards/FIVE_OF_WANDS.JPG'
+import threeWand from './tarot_cards/THREE_OF_WANDS.JPG'
+import twoWand from './tarot_cards/TWO_OF_WANDS.JPG'
+import pageWand from './tarot_cards/PAGE_OF_WANDS.JPG'
+import queenWand from './tarot_cards/QUEEN_OF_WANDS.JPG'
+import kingWand from './tarot_cards/KING_OF_WANDS.JPG'
+import knightWand from './tarot_cards/KNIGHT_OF_WANDS.JPG'
+import kingCup from './tarot_cards/KING_OF_CUPS.JPG'
+import queenCup from './tarot_cards/QUEEN_OF_CUPS.JPG'
+import knightCup from './tarot_cards/KNIGHT_OF_CUPS.JPG'
+import pageCup from './tarot_cards/PAGE_OF_CUPS.JPG'
+import tenCup from './tarot_cards/TEN_OF_CUPS.JPG'
+import nineCup from './tarot_cards/NINE_OF_CUPS.JPG'
+import eightCup from './tarot_cards/EIGHT_OF_CUPS.JPG'
+import sevenCup from './tarot_cards/SEVEN_OF_CUPS.JPG'
+import sixCup from './tarot_cards/SIX_OF_CUPS.JPG'
+import fiveCup from './tarot_cards/FIVE_OF_CUPS.JPG'
+import fourCup from './tarot_cards/FOUR_OF_CUPS.JPG'
+import threeCup from './tarot_cards/THREE_OF_CUPS.JPG'
+import twoCup from './tarot_cards/TWO_OF_CUPS.JPG'
+import aceCup from './tarot_cards/ACE_OF_CUPS.JPG'
+import kingSword from './tarot_cards/KING_OF_SWORDS.JPG'
+import queenSword from './tarot_cards/QUEEN_OF_SWORDS.JPG'
+import knightSword from './tarot_cards/KNIGHT_OF_SWORDS.JPG'
+import pageSword from './tarot_cards/PAGE_OF_SWORDS.JPG'
+import tenSword from './tarot_cards/TEN_OF_SWORDS.JPG'
+import nineSword from './tarot_cards/NINE_OF_SWORDS.JPG'
+import eightSword from './tarot_cards/EIGHT_OF_SWORDS.JPG'
+import sevenSword from './tarot_cards/SEVEN_OF_SWORDS.JPG'
+import sixSword from './tarot_cards/SIX_OF_SWORDS.JPG'
+import fiveSword from './tarot_cards/FIVE_OF_SWORDS.JPG'
+import fourSword from './tarot_cards/FOUR_OF_SWORDS.JPG'
+import threeSword from './tarot_cards/THREE_OF_SWORDS.JPG'
+import twoSword from './tarot_cards/TWO_OF_SWORDS.JPG'
+import aceSword from './tarot_cards/ACE_OF_SWORDS.JPG'
+import kingPent from './tarot_cards/KING_OF_PENTACLES.JPG'
+import queenPent from './tarot_cards/QUEEN_OF_PENTACLES.JPG'
+import knightPent from './tarot_cards/KNIGHT_OF_PENTACLES.JPG'
+import pagePent from './tarot_cards/PAGE_OF_PENTACLES.JPG'
+import tenPent from './tarot_cards/TEN_OF_PENTACLES.JPG'
+import ninePent from './tarot_cards/NINE_OF_PENTACLES.JPG'
+import eightPent from './tarot_cards/EIGHT_OF_PENTACLES.JPG'
+import sevenPent from './tarot_cards/SEVEN_OF_PENTACLES.JPG'
+import sixPent from './tarot_cards/SIX_OF_PENTACLES.JPG'
+import fivePent from './tarot_cards/FIVE_OF_PENTACLES.JPG'
+import fourPent from './tarot_cards/FOUR_OF_PENTACLES.JPG'
+import threePent from './tarot_cards/THREE_OF_PENTACLES.JPG'
+import twoPent from './tarot_cards/TWO_OF_PENTACLES.JPG'
+import acePent from './tarot_cards/ACE_OF_PENTACLES.JPG'
+
+
 const tarotDeck = [
-    "The World",
-    "Judgement",
-    "The Sun",
-    "The Moon",
-    "The Star",
-    "The Tower",
-    "The Devil",
-    "Temperance",
-    "Death",
-    "The Hanged Man",
-    "Justice",
-    "The Wheel of Fortune",
-    "The Hermit",
-    "Strength",
-    "The Chariot",
-    "The Lovers",
-    "The Heirophant",
-    "The Emperor",
-    "The Empress",
-    "The High Priestess",
-    "The Magician",
-    "The Fool",
-    "Seven of Wands",
-    "Four of Wands",
-    "Ace of Wands",
-    "Ten of Wands",
-    "Nine of Wands",
-    "Eight of Wands",
-    "Six of Wands",
-    "Five of Wands",
-    "Three of Wands",
-    "Two of Wands",
-    "Page of Wands",
-    "Queen of Wands",
-    "King of Wands",
-    "Knight of Wands",
-    "King of Cups",
-    "Queen of Cups",
-    "Knight of Cups",
-    "Page of Cups",
-    "Ten of Cups",
-    "Nine of Cups",
-    "Eight of Cups",
-    "Seven of Cups",
-    "Six of Cups",
-    "Five of Cups",
-    "Four of Cups",
-    "Three of Cups",
-    "Two of Cups",
-    "Ace of Cups",
-    "King of Swords",
-    "Knight of Swords",
-    "Queen of Swords",
-    "Page of Swords",
-    "Ten of Swords",
-    "Nine of Swords",
-    "Eight of Swords",
-    "Seven of Swords",
-    "Six of Swords",
-    "Five of Swords",
-    "Three of Swords",
-    "Four of Swords",
-    "Two of Swords",
-    "Ace of Swords",
-    "King of Pentacles",
-    "Queen of Pentacles",
-    "Knight of Pentacles",
-    "Page of Pentacles",
-    "Ten of Pentacles",
-    "Nine of Pentacles",
-    "Eight of Pentacles",
-    "Seven of Pentacles",
-    "Six of Pentacles",
-    "Five of Pentacles",
-    "Four of Pentacles",
-    "Three of Pentacles",
-    "Two of Pentacles",
-    "Ace of Pentacles"
+    {
+        name: "The World",
+        img: world,
+        meaning: "fulfillment, harmony, completion"
+    },
+    {
+        name: "Judgement",
+        img: judgement,
+        meaning: "reflection, reckoning, awakening"
+    },
+    {
+        name: "The Sun",
+        img: sun,
+        meaning: "joy, success, celebration, positivity"
+   },
+    {
+        name: "The Moon",
+        img: moon,
+        meaning: "unconscious, illusions, intuition"
+    },
+    {
+        name: "The Star",
+        img: star,
+        meaning: "hope, faith, rejuvenation"
+    },
+    {
+        name: "The Tower",
+        img: tower,
+        meaning: "sudden upheaval, broken pride, disaster"
+    },
+    {
+        name: "The Devil",
+        img: devil,
+        meaning: "addiction, materialism, playfulness"
+    },
+    {
+        name: "Temperance",
+        img: temperance,
+        meaning: "middle path, patience, finding meaning"
+    },
+    {
+        name: "Death",
+        img: death,
+        meaning: "end of cycle, beginnings, change, metamorphosis"
+    },
+    {
+        name: "The Hanged Man",
+        img: hanged,
+        meaning: "sacrifice, release, martyrdom"
+    },
+    {
+        name: "Justice",
+        img: justice,
+        meaning: "cause and effect, clarity, truth"  
+    },
+    {
+        name: "Wheel of Fortune",
+        img: wheel,
+        meaning: "change, cycles, inevitable fate"
+    },
+    {
+        name: "The Hermit",
+        img: hermit,
+        meaning: "contemplation, search for truth, inner guidance"
+    },
+    {
+        name: "Strength",
+        img: strength,
+        meaning: "inner strength, bravery, compassion, focus"
+    },
+    {
+        name: "The Chariot",
+        img: chariot,
+        meaning: "direction, control, willpower"
+    },
+    {
+        name: "The Lovers",
+        img: lovers,
+        meaning: "partnerships, duality, union"
+    },
+    {
+        name: "The Heirophant",
+        img: heirophant,
+        meaning: "tradition, conformity, morality, ethics"
+    },
+    {
+        name: "The Emperor",
+        img: emperor,
+        meaning: "authority, structure, control, fatherhood"
+    },
+    {
+        name: "The Empress",
+        img: empress,
+        meaning: "motherhood, fertility, nature"
+    },
+    {
+        name: "The High Priestess",
+        img: priestess,
+        meaning: "intuitive, unconscious, inner voice"
+    },
+    {
+        name: "The Magician",
+        img: magician,
+        meaning: "willpower, desire, creation, manifestation"
+    },
+    {
+        name: "The Fool",
+        img: fool,
+        meaning: "innocence, new beginnings, free spirit"
+    },
+    {
+        name: "Seven of Wands",
+        img: sevenWand,
+        meaning: "perseverance, defensive, maintaining control"
+    },
+    {
+        name: "Four of Wands",
+        img: fourWand,
+        meaning: "community, home, celebration"
+    },
+    {
+        name: "Ace of Wands",
+        img: aceWand,
+        meaning: "creation, willpower, inspiration, desire"
+    },
+    {
+        name: "Ten of Wands",
+        img: tenWand,
+        meaning: "accomplishment, responsibility, burden"
+    },
+    {
+        name: "Nine of Wands",
+        img: nineWand,
+        meaning: "resilience, grit, last stand"
+    },
+    {
+        name: "Eight of Wands",
+        img: eightWand,
+        meaning: "rapid action, movement, quick decisions"
+    },
+    {
+        name: "Six of Wands",
+        img: sixWand,
+        meaning: "victory, success, public reward"
+
+    },
+    {
+        name: "Five of Wands",
+        img: fiveWand,
+        meaning: "competition, rivalry, conflict"
+    },
+    {
+        name: "Three of Wands",
+        img: threeWand,
+        meaning: "looking ahead, expansion, rapid growth"
+    },
+    {
+        name: "Two of Wands",
+        img: twoWand,
+        meaning: "planning, making decisions, leaving home"
+    },
+    {
+        name: "Page of Wands",
+        img: pageWand,
+        meaning: "exploration, excitement, freedom"
+    },
+    {
+        name: "Queen of Wands",
+        img: queenWand,
+        meaning: "courage, determination, joy"
+    },
+    {
+        name: "King of Wands",
+        img: kingWand,
+        meaning: "big picture, leader, overcoming challenges"
+    },
+    {
+        name: "Knight of Wands",
+        img: knightWand,
+        meaning: "action, adventure, fearlessness"
+    },
+    {
+        name: "King of Cups",
+        img: kingCup,
+        meaning: "compassion, control, balance"
+    },
+    {
+        name: "Queen of Cups",
+        img: queenCup,
+        meaning: "compassion, calm, comfort"
+    },
+    {
+        name: "Knight of Cups",
+        img: knightCup,
+        meaning: "following the heart, idealist, romantic"
+    },
+    {
+        name: "Page of Cups",
+        img: pageCup,
+        meaning: "happy surprise, dreamer, sensitivity"
+    },
+    {
+        name: "Ten of Cups",
+        img: tenCup,
+        meaning: "inner happiness, fulfillment, dreams coming true"
+    },
+    {
+        name: "Nine of Cups",
+        img: nineCup,
+        meaning: "satisfaction, emotional stability, luxury"
+    },
+    {
+        name: "Eight of Cups",
+        img: eightCup,
+        meaning: "walking away, disillusionment, leaving behind"
+    },
+    {
+        name: "Seven of Cups",
+        img: sevenCup,
+        meaning: "searching for purpose, choices, daydreaming"
+   },
+    {
+        name: "Six of Cups",
+        img: sixCup,
+        meaning: "familiarity, happy memories, healing"
+    },
+    {
+        name: "Five of Cups",
+        img: fiveCup,
+        meaning: "loss, grief, self-pity"
+    },
+    {
+        name: "Four of Cups",
+        img: fourCup,
+        meaning: "apathy, contemplation, disconnectedness"
+    },
+    {
+        name: "Three of Cups",
+        img: threeCup,
+        meaning: "friendship, community, happiness"
+    },
+    {
+        name: "Two of Cups",
+        img: twoCup,
+        meaning: "unity, partnership, connection"
+    },
+    {
+        name: "Ace of Cups",
+        img: aceCup,
+        meaning: "new feelings, spirituality, intuition"
+    },
+    {
+        name: "King of Swords",
+        img: kingSword,
+        meaning: "head over heart, discipline, truth"
+    },
+    {
+        name: "Knight of Swords",
+        img: knightSword,
+        meaning: "action, impulsiveness, defending beliefs"
+    },
+    {
+        name: "Queen of Swords",
+        img: queenSword,
+        meaning: "complexity, perceptiveness, clear mindedness"
+    },
+    {
+        name: "Page of Swords",
+        img: pageSword,
+        meaning: "curiosity, restlessness, mental energy"
+    },
+    {
+        name: "Ten of Swords",
+        img: tenSword,
+        meaning: "failure, collapse, defeat"
+    },
+    {
+        name: "Nine of Swords",
+        img: nineSword,
+        meaning: "anxiety, hopelessness, trauma"
+    },
+    {
+        name: "Eight of Swords",
+        img: eightSword,
+        meaning: "imprisonment, entrapment, self-victimization"
+    },
+    {
+        name: "Seven of Swords",
+        img: sevenSword,
+        meaning: "deception, trickery, tactics and strategy"
+    },
+    {
+        name: "Six of Swords",
+        img: sixSword,
+        meaning: "transition, leaving behind, moving on"
+    },
+    {
+        name: "Five of Swords",
+        img: fiveSword,
+        meaning: "unbridled ambition, win at all costs, sneakiness"    
+    },
+    {
+        name: "Three of Swords",
+        img: threeSword,
+        meaning: "heartbreak, suffering, grief"
+    },
+    {
+        name: "Four of Swords",
+        img: fourSword,
+        meaning: "rest, restoration, contemplation"    
+    },
+    {
+        name: "Two of Swords",
+        img: twoSword,
+        meaning: "difficult choices, indecision, stalemate" 
+    },
+    {
+        name: "Ace of Swords",
+        img: aceSword,
+        meaning: "breakthrough, clarity, sharp mind"
+    },
+    {
+        name: "King of Pentacles",
+        img: kingPent,
+        meaning: "abundance, prosperity, security"
+    },
+    {
+        name: "Queen of Pentacles",
+        img: queenPent,
+        meaning: "practicality, creature comforts, financial security"
+    },
+    {
+        name: "Knight of Pentacles",
+        img: knightPent,
+        meaning: "efficiency, hard work, responsibility"
+    },
+    {
+        name: "Page of Pentacles",
+        img: pagePent,
+        meaning: "ambition, desire, diligence"    
+    },
+    {
+        name: "Ten of Pentacles",
+        img: tenPent,
+        meaning: "legacy, culmination, inheritance"  
+    },
+    {
+        name: "Nine of Pentacles",
+        img: ninePent,
+        meaning: "fruits of labor, rewards, luxury"
+
+    },
+    {
+        name: "Eight of Pentacles",
+        img: eightPent,
+        meaning: "apprenticeship, passion, high standards"
+    },
+    {
+        name: "Seven of Pentacles",
+        img: sevenPent,
+        meaning: "hard work, perseverance, diligence"
+    },
+    {
+        name: "Six of Pentacles",
+        img: sixPent,
+        meaning: "charity, generosity, sharing" 
+    },
+    {
+        name: "Five of Pentacles",
+        img: fivePent,
+        meaning: "need, poverty, insecurity"    
+    },
+    {
+        name: "Four of Pentacles",
+        img: fourPent,
+        meaning: "conservation, frugality, security"
+    },
+    {
+       name: "Three of Pentacles",
+       img: threePent,
+       meaning: "teamwork, collaboration, building"
+    },
+    {
+        name: "Two of Pentacles",
+        img: twoPent,
+        meaning: "balancing decisions, priorities, adapting to change"
+    },
+    {
+       name: "Ace of Pentacles",
+       img: acePent,
+       meaning: "opportunity, prosperity, new venture"
+    }
 ]
 
 export default tarotDeck
--- src/Components/Deck/Deck.js
+++ src/Components/Deck/Deck.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import tarotDeck from './Deck-array'
 
+// const PATH = './tarot_cards/'
+
 function Deck() {
     const [deck, setDeck] = useState(tarotDeck)
-    const [card, setCard] = useState("")  
-    
+    const [card, setCard] = useState("") 
+    // const [image, setImage] = useState("")
+
     function getRandomInt(max) {
         return(Math.round(Math.random() * (max-1)))
     }
@@ -12,8 +15,9 @@ function Deck() {
     function getCard() {
         const index = getRandomInt(deck.length);
         setCard(deck[index])
-        console.log("deck now has " + deck.length)
+        // const postImg = require('./tarot_cards/' + card.img)
     }
+
 
     useEffect(() => {
         setDeck(prev => prev.filter(crd => crd !== card))
@@ -28,9 +32,12 @@ function Deck() {
     
     return (
         <div>
-           {card ? <h1>You got {card}!</h1> : <h1>Click the button to pick a card</h1>}
+           {card ? <h1>You got {card.name}! </h1> : <h1>Click the button to pick a card</h1>}
            <button onClick={getCard}>Get a Card</button>
            <button onClick={resetDeck}>Reset Deck</button>
+           {/* <div>{card && card.img ? <img src={require(`${PATH}${card.img}`)} alt={card.name}/> : ""}</div> */}
+           <div>{card && card.img ? <img src={card.img} alt={card.name}/> : ""}</div>
+           <div>{card.meaning ? <div>{card.name} means {card.meaning} when upright!</div> : ""}</div>
         </div>
     )
 }
